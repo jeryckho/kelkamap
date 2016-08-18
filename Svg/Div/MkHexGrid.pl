b345587e480827effee14902a9e6a98254c996d3
Jerome <chery.jerome@online.fr>
2016-08-18 22:54:35 +0200

    Grid

---
+++ Svg/Div/MkHexGrid.pl
@@ -0,0 +1,43 @@
+use Math::Trig;
+# use Math::Round;
+
+my $tpl = '<polygon class="H" id="H%02d%02d" points="%s"/>';
+
+my $Size = 6000/82;
+my $Vert = $Size * sqrt(3);
+my $W = 6000;
+my $H = 6000;
+
+# $x = tan(0.9);
+# $y = acos(3.7);
+# $z = asin(2.4);
+# $halfpi = pi/2;
+# $rad = deg2rad(120);
+
+print '<?xml version="1.0" encoding="UTF-8" standalone="no"?>'."\n<svg>\n";
+
+$dx = 0;
+for(my $x = 0; $x < $W; $x += 1.5 * $Size) {
+
+	my( $dV ) = ($dx % 2) == 0 ? 0 :  $Vert/2;
+	for(my $y = 0; $y < $H; $y += $Vert) {
+		printf( $tpl, $dx, $dy, hexPoints( $x, $y + $dV, $Size ) );
+		$dy++;
+	}
+	print "\n";
+	$dy = 0;
+	$dx++;
+}
+print "</svg>\n";
+
+exit(0);
+
+sub hexPoints ($$$) {
+	my ($cx,$cy,$r) = @_;
+	my (@pts) = ();
+
+	for(my $i=0;$i<6;$i++) {
+		push( @pts, int( 0.5 + 1000 * ( $cx + $r * cos( deg2rad( 60 * $i ) ) ) )/1000, int( 0.5 + 1000 * ( $cy + $r * sin( deg2rad( 60 * $i ) ) ) )/1000 ) ;
+	}
+	return join(' ', @pts);
+}
--- Svg/src/js/directives.js
+++ Svg/src/js/directives.js
@@ -3,7 +3,7 @@ angular.module('SvgMapApp').directive('svgMap', ['$compile', function ($compile)
         restrict: 'A',
         templateUrl: 'img/FondCarte3.svg',
         link: function (scope, element, attrs) {
-            var regions = element[0].querySelectorAll('.Z');
+            var regions = element[0].querySelectorAll('.H');
             angular.forEach(regions, function (path, key) {
                 var regionElement = angular.element(path);
                 regionElement.attr("region", "");
