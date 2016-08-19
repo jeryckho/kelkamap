1d081167599a34775cd3125446fb6bb396c0e153
Jerome Chery <jerome.chery@nrco.fr>
2016-08-19 15:00:14 +0200

    Zoom And Pan

--- Svg/src/js/controllers.js
+++ Svg/src/js/controllers.js
@@ -1,19 +1,55 @@
 angular.module('SvgMapApp', ['angular-loading-bar'])
-    .controller('MainCtrl', ['$scope', function ($scope) {
-		  $scope.ngViewBox = "-2640 -2520 5880 5880";
+	.controller('MainCtrl', ['$scope', function ($scope) {
+		$scope.vBox = {
+			Min: { x: -2640, y: -2520, w: 980, h: 980 },
+			Max: { x: 3240, y: 3360, w: 5880, h: 5880 },
+			Crt: { x: -2640, y: -2520, w: 5880, h: 5880 }
+		};
 		//   $scope.ngViewBox = "100 100 1230 870";
-        var states = [
-				"Terres","Autre"];
-        $scope.createDummyData = function () {
-            var dataTemp = {};
-            angular.forEach(states, function (state, key) {
-                dataTemp[state] = {value: Math.random()}
-            });
-            $scope.dummyData = dataTemp;
-        };
-        $scope.createDummyData();
+		var states = ["Terres", "Autre"];
 
-        $scope.changeHoverRegion = function (region) {
-            $scope.hoverRegion = region;
-        };
-    }]);
+		$scope.createDummyData = function () {
+			var dataTemp = {};
+			angular.forEach(states, function (state, key) {
+				dataTemp[state] = { value: Math.random() }
+			});
+			$scope.dummyData = dataTemp;
+		};
+
+		$scope.changeHoverRegion = function (region) {
+			$scope.hoverRegion = region;
+		};
+
+		$scope.toZoom = function (fac,bZoom) {
+			if (bZoom) {
+				$scope.vBox.Crt.w /= fac;
+				$scope.vBox.Crt.h /= fac;
+			} else {
+				$scope.vBox.Crt.w *= fac;
+				$scope.vBox.Crt.h *= fac;
+			}
+			$scope.setBox();
+		}
+
+		$scope.toPan = function (mW,mH) {
+			$scope.vBox.Crt.x += mW * $scope.vBox.Crt.w;
+			$scope.vBox.Crt.y += mH * $scope.vBox.Crt.h;
+			$scope.setBox();
+		}
+
+		$scope.setBox = function () {
+			if ( $scope.vBox.Crt.w > $scope.vBox.Max.w ) $scope.vBox.Crt.w = $scope.vBox.Max.w;
+			if ( $scope.vBox.Crt.h > $scope.vBox.Max.h ) $scope.vBox.Crt.h = $scope.vBox.Max.h;
+			if ( $scope.vBox.Crt.w < $scope.vBox.Min.w ) $scope.vBox.Crt.w = $scope.vBox.Min.w;
+			if ( $scope.vBox.Crt.h < $scope.vBox.Min.h ) $scope.vBox.Crt.h = $scope.vBox.Min.h;
+
+			if ( $scope.vBox.Crt.x < $scope.vBox.Min.x ) $scope.vBox.Crt.x = $scope.vBox.Min.x;
+			if ( $scope.vBox.Crt.y < $scope.vBox.Min.y ) $scope.vBox.Crt.y = $scope.vBox.Min.y;
+			if ( $scope.vBox.Crt.x + $scope.vBox.Crt.w > $scope.vBox.Max.x ) $scope.vBox.Crt.x = $scope.vBox.Max.x - $scope.vBox.Crt.w;
+			if ( $scope.vBox.Crt.y + $scope.vBox.Crt.h > $scope.vBox.Max.y ) $scope.vBox.Crt.y = $scope.vBox.Max.y - $scope.vBox.Crt.h;
+			$scope.ngViewBox = $scope.vBox.Crt.x + " " + $scope.vBox.Crt.y + " " + $scope.vBox.Crt.w + " " + $scope.vBox.Crt.h;
+		};
+
+		$scope.createDummyData();
+		$scope.setBox();
+	}]);
--- Svg/src/js/directives.js
+++ Svg/src/js/directives.js
@@ -25,14 +25,15 @@ angular.module('SvgMapApp').directive('region', ['$compile', function ($compile)
         link: function (scope, element, attrs) {
             scope.elementId = element.attr("id");
             scope.regionClick = function () {
-                alert(scope.dummyData[scope.elementId].value);
+               //  alert(scope.dummyData[scope.elementId].value);
+					alert(scope.elementId);
             };
             scope.regionMouseOver = function () {
                 scope.hoverRegion = scope.elementId;
-                element[0].parentNode.appendChild(element[0]);
+               //  element[0].parentNode.appendChild(element[0]);	
             };
             element.attr("ng-click", "regionClick()");
-            element.attr("ng-attr-fill", "{{dummyData[elementId].value | map_colour}}");
+            // element.attr("ng-attr-fill", "{{dummyData[elementId].value | map_colour}}");
             element.attr("ng-mouseover", "regionMouseOver()");
             element.attr("ng-class", "{active:hoverRegion==elementId}");
             element.removeAttr("region");
