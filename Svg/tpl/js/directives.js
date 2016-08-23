1a3e4a15778e9ec4561ce591efd51b451705b126
Jerome Chery <jerome.chery@nrco.fr>
2016-08-23 13:36:21 +0200

    structure js

--- Svg/tpl/js/directives.js
+++ Svg/tpl/js/directives.js
@@ -1,88 +1,89 @@
-angular.module('SvgMapApp').directive('svgMap', ['$compile', function ($compile) {
-    return {
-        restrict: 'A',
-        templateUrl: 'img/KelkaMap.svg',
-        link: function (scope, element, attrs) {
-            var regions = element[0].querySelectorAll('.H');
-            angular.forEach(regions, function (path, key) {
-                var regionElement = angular.element(path);
-                regionElement.attr("region", "");
-                regionElement.attr("hover-region", "hoverRegion");
-                $compile(regionElement)(scope);
-            })
-        }
-    }
-}]);
-
-angular.module('SvgMapApp').directive('svgGrid', ['$compile', function ($compile) {
-    return {
-        restrict: 'A',
-        templateUrl: 'img/KelkaGrid.svg',
-        link: function (scope, element, attrs) {
-            var regions = element[0].querySelectorAll('.H');
-            angular.forEach(regions, function (path, key) {
-                var regionElement = angular.element(path);
-                regionElement.attr("region", "");
-                regionElement.attr("hover-region", "hoverRegion");
-                $compile(regionElement)(scope);
-            })
-        }
-    }
-}]);
-
-angular.module('SvgMapApp').directive('region', ['$compile', 'DataSvc', function ($compile, DataSvc) {
-    return {
-        restrict: 'A',
-        scope: {
-            hoverRegion: "="
-        },
-        link: function (scope, element, attrs) {
-            scope.elementId = element.attr("id");
-				scope.DataSvc = DataSvc;
-            scope.regionClick = function () {
-					// alert(scope.elementId);
-                scope.hoverRegion = scope.elementId;
-					 scope.DataSvc.Map.Focus = {};
-					 scope.DataSvc.Map.FocusNation = "";					 
-					 if (angular.isDefined(scope.DataSvc.Map.Hexagones[scope.hoverRegion])) {
-						 scope.DataSvc.Map.FocusNation = scope.DataSvc.Map.Hexagones[scope.hoverRegion];
-						 var Lst = scope.DataSvc.Map.Nations[scope.DataSvc.Map.Hexagones[scope.hoverRegion]].Hexagones;
-						 angular.forEach(Lst, function (elem) {
-							scope.DataSvc.Map.Focus[elem] = true;
-						 });
-					 }
-            };
-            scope.regionMouseOver = function () {
-                scope.hoverRegion = scope.elementId;
-					 scope.DataSvc.Map.Focus = {};
-					 scope.DataSvc.Map.FocusNation = "";					 
-					 if (angular.isDefined(scope.DataSvc.Map.Hexagones[scope.hoverRegion])) {
-						 scope.DataSvc.Map.FocusNation = scope.DataSvc.Map.Hexagones[scope.hoverRegion];
-						 var Lst = scope.DataSvc.Map.Nations[scope.DataSvc.Map.Hexagones[scope.hoverRegion]].Hexagones;
-						 angular.forEach(Lst, function (elem) {
-							scope.DataSvc.Map.Focus[elem] = true;
-						 });
-					 }
-               //  element[0].parentNode.appendChild(element[0]);	
-            };
-            element.attr("ng-click", "regionClick()");
-            element.attr("ng-mouseover", "regionMouseOver()");
-            element.attr("ng-class", "{active:hoverRegion==elementId,nation:DataSvc.Map.Focus[elementId]}");
-            element.removeAttr("region");
-            $compile(element)(scope);
-        }
-    }
-}]);
-
-angular.module('SvgMapApp').directive('viewBox', [
-	function () {
-		'use strict';
+angular.module('SvgMapApp')
+	.directive('svgMap', ['$compile', function ($compile) {
 		return {
-			link: function (scope, element, attributes) {
-				attributes.$observe('viewBox', function(value) {
-					element.attr('viewBox', value);
-				});
+			restrict: 'A',
+			templateUrl: 'img/KelkaMap.svg',
+			link: function (scope, element, attrs) {
+				var regions = element[0].querySelectorAll('.H');
+				angular.forEach(regions, function (path, key) {
+					var regionElement = angular.element(path);
+					regionElement.attr("region", "");
+					regionElement.attr("hover-region", "hoverRegion");
+					$compile(regionElement)(scope);
+				})
 			}
-		};
-	}
-]);
+		}
+	}])
+
+	.directive('svgGrid', ['$compile', function ($compile) {
+		return {
+			restrict: 'A',
+			templateUrl: 'img/KelkaGrid.svg',
+			link: function (scope, element, attrs) {
+				var regions = element[0].querySelectorAll('.H');
+				angular.forEach(regions, function (path, key) {
+					var regionElement = angular.element(path);
+					regionElement.attr("region", "");
+					regionElement.attr("hover-region", "hoverRegion");
+					$compile(regionElement)(scope);
+				})
+			}
+		}
+	}])
+
+	.directive('region', ['$compile', 'DataSvc', function ($compile, DataSvc) {
+		return {
+			restrict: 'A',
+			scope: {
+				hoverRegion: "="
+			},
+			link: function (scope, element, attrs) {
+				scope.elementId = element.attr("id");
+				scope.DataSvc = DataSvc;
+				scope.regionClick = function () {
+					// alert(scope.elementId);
+					scope.hoverRegion = scope.elementId;
+					scope.DataSvc.Map.Focus = {};
+					scope.DataSvc.Map.FocusNation = "";
+					if (angular.isDefined(scope.DataSvc.Map.Hexagones[scope.hoverRegion])) {
+						scope.DataSvc.Map.FocusNation = scope.DataSvc.Map.Hexagones[scope.hoverRegion];
+						var Lst = scope.DataSvc.Map.Nations[scope.DataSvc.Map.Hexagones[scope.hoverRegion]].Hexagones;
+						angular.forEach(Lst, function (elem) {
+							scope.DataSvc.Map.Focus[elem] = true;
+						});
+					}
+				};
+				scope.regionMouseOver = function () {
+					scope.hoverRegion = scope.elementId;
+					scope.DataSvc.Map.Focus = {};
+					scope.DataSvc.Map.FocusNation = "";
+					if (angular.isDefined(scope.DataSvc.Map.Hexagones[scope.hoverRegion])) {
+						scope.DataSvc.Map.FocusNation = scope.DataSvc.Map.Hexagones[scope.hoverRegion];
+						var Lst = scope.DataSvc.Map.Nations[scope.DataSvc.Map.Hexagones[scope.hoverRegion]].Hexagones;
+						angular.forEach(Lst, function (elem) {
+							scope.DataSvc.Map.Focus[elem] = true;
+						});
+					}
+					//  element[0].parentNode.appendChild(element[0]);	
+				};
+				element.attr("ng-click", "regionClick()");
+				element.attr("ng-mouseover", "regionMouseOver()");
+				element.attr("ng-class", "{active:hoverRegion==elementId,nation:DataSvc.Map.Focus[elementId]}");
+				element.removeAttr("region");
+				$compile(element)(scope);
+			}
+		}
+	}])
+
+	.directive('viewBox', [
+		function () {
+			'use strict';
+			return {
+				link: function (scope, element, attributes) {
+					attributes.$observe('viewBox', function (value) {
+						element.attr('viewBox', value);
+					});
+				}
+			};
+		}
+	]);
--- Svg/tpl/js/services.js
+++ Svg/tpl/js/services.js
@@ -1,29 +1,29 @@
-angular.module('SvgMapApp').factory('DataSvc', ['$http', function ($http) {
-	var svc = {};
+angular.module('SvgMapApp')
+	.factory('DataSvc', ['$http', function ($http) {
+		var svc = {};
 
-	$http.get('data/DataSvc.json').success(function (data) {
-		svc.Map = data;
-	});
+		$http.get('data/DataSvc.json').success(function (data) {
+			svc.Map = data;
+		});
 
-	return svc;
-}]);
+		return svc;
+	}])
 
-angular.module('SvgMapApp').factory('FbSvc', ['$firebaseAuth', function ($firebaseAuth) {
-	var svc = {};
-	svc.auth = $firebaseAuth();
+	.factory('FbSvc', ['$firebaseAuth', function ($firebaseAuth) {
+		var svc = {};
+		svc.auth = $firebaseAuth();
 
-	svc.login = function (lg, mp) {
-      return svc.auth.$signInWithEmailAndPassword(lg, mp);
-	}
+		svc.login = function (lg, mp) {
+			return svc.auth.$signInWithEmailAndPassword(lg, mp);
+		}
 
-	svc.logout = function () {
-      svc.auth.$signOut();
-	}
+		svc.logout = function () {
+			svc.auth.$signOut();
+		}
 
-	svc.isLoggedIn = function () {
-      return svc.auth.$getAuth();
-	}
+		svc.isLoggedIn = function () {
+			return svc.auth.$getAuth();
+		}
 
-	return svc;
-}]);
-
+		return svc;
+	}]);
