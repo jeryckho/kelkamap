da795bbecccb137b4d3de66e4d95c9a61703a9f3
Jerome Chery <jerome.chery@nrco.fr>
2016-08-25 11:52:31 +0200

    Accès partie admin

--- Svg/tpl/js/controllers.js
+++ Svg/tpl/js/controllers.js
@@ -1,5 +1,13 @@
 angular.module('SvgMapApp', ['angular-loading-bar', 'firebase', 'ngRoute'])
 
+	.run(['$rootScope', '$location', function ($rootScope, $location) {
+		$rootScope.$on('$routeChangeError', function (event, next, previous, error) {
+			if (error === 'AUTH_REQUIRED') {
+				$location.path('/Log');
+			}
+		});
+	}])
+
 	.config(['$routeProvider', function ($routeProvider) {
 		$routeProvider
 			.when('/', {
@@ -13,6 +21,11 @@ angular.module('SvgMapApp', ['angular-loading-bar', 'firebase', 'ngRoute'])
 			.when('/Adm', {
 				templateUrl: 'html/Adm.html',
 				controller: 'AdmCtrl',
+				resolve: {
+					'currentAuth': ['Auth', function (Auth) {
+						return Auth.auth.$requireSignIn();
+					}]
+				}
 			})
 			.otherwise({ redirectTo: '/' });
 	}])
@@ -26,24 +39,19 @@ angular.module('SvgMapApp', ['angular-loading-bar', 'firebase', 'ngRoute'])
 		$scope.Zoom.setBox();
 	}])
 
-	.controller('AdmCtrl', ['$scope', function ($scope) {
-		$scope.cnt = "ADM";
+	.controller('AdmCtrl', ['$scope', 'currentAuth', function ($scope, currentAuth) {
+		$scope.cnt = 'ADM';
 	}])
 
-	.controller('LogCtrl', ['$scope', function ($scope) {
-		$scope.cnt = "LOG";
-	}])
-
-	.controller('InfoCtrl', ['$scope', 'Data', 'Auth', 'Zoom', function ($scope, Data, Auth, Zoom) {
-
-		$scope.Data = Data;
+	.controller('LogCtrl', ['$scope', 'Auth', '$location', function ($scope, Auth, $location) {
 		$scope.Auth = Auth;
-		$scope.Zoom = Zoom;
+		$scope.cnt = 'LOG';
 
 		$scope.connect = function () {
 			$scope.Auth.login($scope.alog, $scope.apwd)
 				.then(function (data) {
-					$scope.cnt = "OK";
+					$scope.cnt = 'OK';
+					$location.path('/Adm');
 				})
 				.catch(function (error) {
 					$scope.cnt = error;
@@ -52,10 +60,17 @@ angular.module('SvgMapApp', ['angular-loading-bar', 'firebase', 'ngRoute'])
 		$scope.unlog = function () {
 			$scope.Auth.logout()
 				.then(function (data) {
-					$scope.cnt = "-";
+					$scope.cnt = '-';
+					$location.path('/');
 				})
 				.catch(function (error) {
 					$scope.cnt = error;
 				});
 		}
+	}])
+
+	.controller('InfoCtrl', ['$scope', 'Data', 'Auth', 'Zoom', function ($scope, Data, Auth, Zoom) {
+		$scope.Auth = Auth;
+		$scope.Data = Data;
+		$scope.Zoom = Zoom;
 	}]);
