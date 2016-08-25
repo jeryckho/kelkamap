8cbf20d4bda0d4629df3f85ed95c56029b43a2f6
Jerome Chery <jerome.chery@nrco.fr>
2016-08-25 10:54:07 +0200

    Routes

--- Svg/tpl/js/controllers.js
+++ Svg/tpl/js/controllers.js
@@ -1,11 +1,39 @@
-angular.module('SvgMapApp', ['angular-loading-bar', 'firebase'])
-	.controller('MainCtrl', ['$scope', 'Data', 'Zoom', function ($scope, Data, Zoom) {
+angular.module('SvgMapApp', ['angular-loading-bar', 'firebase', 'ngRoute'])
+
+	.config(['$routeProvider', function ($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'html/Info.html',
+				controller: 'InfoCtrl',
+			})
+			.when('/Log', {
+				templateUrl: 'html/Log.html',
+				controller: 'LogCtrl',
+			})
+			.when('/Adm', {
+				templateUrl: 'html/Adm.html',
+				controller: 'AdmCtrl',
+			})
+			.otherwise({ redirectTo: '/' });
+	}])
+
+
+	.controller('ImgCtrl', ['$scope', 'Data', 'Zoom', function ($scope, Data, Zoom) {
 
 		$scope.Data = Data;
 		$scope.Zoom = Zoom;
 
 		$scope.Zoom.setBox();
 	}])
+
+	.controller('AdmCtrl', ['$scope', function ($scope) {
+		$scope.cnt = "ADM";
+	}])
+
+	.controller('LogCtrl', ['$scope', function ($scope) {
+		$scope.cnt = "LOG";
+	}])
+
 	.controller('InfoCtrl', ['$scope', 'Data', 'Auth', 'Zoom', function ($scope, Data, Auth, Zoom) {
 
 		$scope.Data = Data;
